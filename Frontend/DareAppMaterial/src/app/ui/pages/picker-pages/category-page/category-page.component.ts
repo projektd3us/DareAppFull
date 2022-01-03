3d385793314047ccf362e554f121bd3d5834b3bc
projektd3us <71829402+projektd3us@users.noreply.github.com>
2022-01-03 06:15:11 +0200

    styling and fun with design

--- Frontend/DareAppMaterial/src/app/ui/pages/picker-pages/category-page/category-page.component.ts
+++ Frontend/DareAppMaterial/src/app/ui/pages/picker-pages/category-page/category-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ActivatedRoute, Router } from '@angular/router';
+import { InfoPageComponent } from '../../info-page/info-page.component';
+import { LoginPageComponent } from '../../login-page/login-page.component';
 
 @Component({
   selector: 'app-category-page',
@@ -7,12 +10,13 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./category-page.component.css']
 })
 export class CategoryPageComponent implements OnInit {
-  constructor(private router:ActivatedRoute) { 
+  constructor(private router:ActivatedRoute, private _bottomSheet: MatBottomSheet) { 
 
   }
   orderObj: any= '';
   categoryTitle: string = '';
   categoryColor: string = '';
+  categoryPic: string = '';
 
   ngOnInit(): void {
     this.router.queryParams.subscribe((i) => {
@@ -21,10 +25,20 @@ export class CategoryPageComponent implements OnInit {
 
     this.categoryTitle = this.orderObj.replace('_', ' '); // machine_generated -> machine generated
     this.categoryColor = this.orderObj.replace('_', '-')+ '-color'; // machine_generated -> machine-generated-color
+    this.categoryPic = this.orderObj.replace('_', '-');
 
     console.log(this.categoryTitle);
     console.log(this.categoryColor);
+    console.log(this.categoryPic);
 
   }
 
+  openLoginPage(): void {
+    this._bottomSheet.open(LoginPageComponent);
+  }
+
+
+  openInfoPage(): void {
+    this._bottomSheet.open(InfoPageComponent);
+  }
 }
